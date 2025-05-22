5b59f1e6702aec91bab3c7d6c9f686734e5187f5
abhisheko5 <86157818+abhisheko5@users.noreply.github.com>
2025-05-22 16:29:01 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import cookieparser from "cookie-par"
 
 dotenv.config();
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.port || 8000,()=>{
+       console.log(`server is running at port: ${process.env.port}`);  
+    })
+})
+.catch((err)=>{
+    console.log("mongo DB connection failed",err);
+})
 
 
 
-/*(async ()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`)
-        app.on("error",(error)=>{
-            console.log("error:",error);
-        })
-
-        app.listen(process.env.port ,()=>{
-            console.log(`App is listening on port${process.env.port}`);
-        })
-    }
-    catch(error){
-        console.error("error :", error)
-        throw err;
-
-    }
-    
-})()*/
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,28 @@
+const asynchandlers=(requesthandler)=>{
+    (req,res,next)=>{
+        promise.resolve(requesthandler(req,res,next)).
+        catch((err)=>next(err));
+    }
+}
+
+
+
+
+
+
+const asynchandler=(func)=> async(req,res,next)=>{
+    try{
+
+        await func(req,res,next)
+
+    }
+    catch(error){
+        res.status(err.code || 400).json({
+            success:false,
+            message:err.message
+        })
+    }
+
+}
+
+export {asynchandler}
